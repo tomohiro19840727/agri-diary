50c5b69541aef83e2708fea752ca74133427635d
tomohiro <tomohirofarm@gmail.com>
2023-12-08 09:24:52 +0900

    11

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ function App() {
   return (
     <>
     <div className="container mx-auto mt-8 text-center">
-      <h1 className="text-4xl font-bold mb-4">日誌アプリ</h1>
+      <h1 className="text-4xl font-bold mb-4">日誌アプリだよ</h1>
       <div className="flex justify-center items-center mb-4">
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-4 rounded"
